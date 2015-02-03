518d2e7ae073e203fbc3c893ab37314cec155f9d
AQ <aq@okaq.com>
2015-02-03 14:49:35 -0500

    New file woku.html. Oxy font layout and render

--- ioma/web.go
+++ ioma/web.go
@@ -12,12 +12,13 @@ const (
     // SHIN = "shin.html" // new
     // TETO = "teto.html" // 1fps diffusion
     // UMAO = "umao.html" // voronoise
-    VANI = "vani.html" // peerjs test
+    // VANI = "vani.html" // peerjs test
+    WOKU = "woku.html" // bitmap oxy font layout and render
     PEERJS = "peer.min.js" // PeerJS
 )
 
 func SelectorHandler(w http.ResponseWriter, req *http.Request) {
-    http.ServeFile(w, req, VANI)
+    http.ServeFile(w, req, WOKU)
 }
 
 func PeerJSHandler(w http.ResponseWriter, req *http.Request) {
