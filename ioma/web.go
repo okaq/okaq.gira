60e4483432f28f08cf818a504084e5b112dbafbc
AQ <aq@okaq.com>
2015-02-05 13:19:25 -0500

    Sync web counter test

--- ioma/web.go
+++ ioma/web.go
@@ -4,7 +4,9 @@
 package main
 
 import (
+    "fmt"
     "net/http"
+    "sync"
 )
 
 const (
@@ -16,10 +18,44 @@ const (
     // WOKU = "woku.html" // bitmap oxy font layout and render
     YOMI = "yomi.html" // server side peer id distribution
     PEERJS = "peer.min.js" // PeerJS
+    // apiKey
 )
 
+var (
+    C *Counter
+)
+
+type Counter struct {
+    Id int
+    sync.Mutex
+}
+
+func NewCounter() *Counter {
+    C := new(Counter)
+    C.Id = 0
+    return C
+}
+
+func (c *Counter) Increment() {
+    c.Lock()
+    defer c.Unlock()
+    c.Id = c.Id + 1
+}
+
+// struct RWLock peerid counter, cycles from (0-31)
+// client can merely try to connect to [id-1,id+1]
+// json struct, {apiKey,peerId}
+
 func SelectorHandler(w http.ResponseWriter, req *http.Request) {
+    // C.Increment()
+    // fmt.Printf("Web Count: %d\n", C.Id)
     http.ServeFile(w, req, YOMI)
+}
+
+func CountHandler(w http.ResponseWriter, req *http.Request) {
+    C.Increment()
+    s0 := fmt.Sprintf("Web Count: %d\n", C.Id)
+    w.Write([]byte(s0))
 }
 
 func PeerJSHandler(w http.ResponseWriter, req *http.Request) {
@@ -27,6 +63,8 @@ func PeerJSHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func init() {
+    C = NewCounter()
     http.HandleFunc("/", SelectorHandler)
+    http.HandleFunc("/count", CountHandler);
     http.HandleFunc("/peer.min.js", PeerJSHandler)
 }
