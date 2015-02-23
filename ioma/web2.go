bd25daa86fa12b07120ef22411e3b00acf639bf2
AQ <aq@okaq.com>
2015-02-23 09:14:23 -0500

    New file zumo.html

--- ioma/web2.go
+++ ioma/web2.go
@@ -17,6 +17,7 @@ const (
     NAKO = "nako.html" // bmp ping pong
     OTOH = "otoh.html" // 1fps binary bitmap anim
     RUKI = "ruki.html" // bezier curve alpha
+    ZUMO = "zumo.html" // partizan test
 )
 
 var (
@@ -60,7 +61,7 @@ func (p *Peers) Json() ([]byte, error) {
 // root static html handle
 // single client html5 web app for xhr, net, conn
 func RootHandler(w http.ResponseWriter, req *http.Request) {
-    http.ServeFile(w, req, RUKI)
+    http.ServeFile(w, req, ZUMO)
 }
 
 // peers cache ajax response
