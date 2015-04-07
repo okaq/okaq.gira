7cd2394f78b0752bbc56f4b9f222865c0dd8f655
AQ <aq@okaq.com>
2015-04-07 15:04:06 -0400

    Xhr test data message

--- karu/web.go
+++ karu/web.go
@@ -16,4 +16,11 @@ func init() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         http.ServeFile(w, r, BADE)
     })
+    http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("test ok if you can read this"))
+    })
 }
+
+// may initially seed root request handler
+// with template that emits server info, token
+
