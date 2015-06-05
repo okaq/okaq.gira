1718db5f2d724d4cf5f94d4c66431fa7ae14e16b
AQ <aq@okaq.com>
2015-06-05 17:51:00 -0400

    New file web.go. Golang live web server

--- karu/web.go
+++ karu/web.go
@@ -1,191 +1,26 @@
-// golang web server 
-// dev env
+// golang web server
+// gcloud dev env
 // AQ <aq@okaq.com>
-// 2015-04-04
+// 2015-06-06
 package main
 
 import (
-    "bytes"
-    "fmt"
     "net/http"
-    "os"
 )
 
 const (
-    BADE = "bade.html"
-    CAPI = "capi.html"
-    ENKO = "enko.html"
-    FAGO = "fago.html"
-    GUGO = "gugo.html"
-    HOKA = "hoka.html"
-    IONU = "ionu.html"
-    JONU = "jonu.html"
-    KONU = "konu.html"
-    LONU = "lonu.html"
     MONU = "monu.html"
 )
 
-var (
-    CS *Capis
-)
-
-// capi store
-// if file not found create it
-// load it on first handle
-// write on change of state
-// format : streaming json encoding
-type Capis struct {
-    // filename string
-    Name string
-    // *os.File
-    File *os.File
-    // sync.RWLock
-}
-
-func NewCapis() *Capis {
-    c0 := new(Capis)
-    c0.Name = "dapi.json"
-    var err error
-    if (c0.Exists()) {
-        c0.File, err = os.Open(c0.Name)
-        if err != nil {
-            panic("cannot open file")
-        }
-    } else {
-        c0.File, err = os.Create(c0.Name)
-        if err != nil {
-            panic("cannot create file")
-            // always panics in app engine
-            // distributed filesystem
-            // does not allow file creation
-        }
-    }
-    return c0
-}
-
-func (c *Capis) Exists() bool {
-    if _, err := os.Stat(c.Name); err != nil {
-        if os.IsNotExist(err) {
-            return false
-        }
-    }
-    return true
-}
-// use blobstore api for large file upload
-// datastore for persistent sql
-// memcache for short lived globals memory
-
-// go-fsnotify for file notifications
-
-func CapiHandler(w http.ResponseWriter, r *http.Request) {
-    // http.ServeFile(w, r, CAPI)
-    // open file (rwlock'd)
-    // save init state
-    w.Write([]byte(CS.Name))
-}
-
-// server side events streaming server
-// response mime type "text/event-stream"
-func CapiSseHandler(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "text/event-stream")
-    // w.Write([]byte("event: message\ndata: ok stream!\n\n"))
-    // w.Write([]byte("data: ok stream!"))
-    if f, ok := w.(http.Flusher); ok {
-        // write seems to close the conn
-        // throwing a client side error
-        // if Flush is implemented
-        fmt.Fprintf(w, "event: message\n")
-        f.Flush()
-        for i := 0; i < 16; i++ {
-            fmt.Fprintf(w, "data: ok stream #%d\n\n", i)
-            f.Flush()
-        }
-        // json format
-        // can also use golang encoder
-        fmt.Fprintf(w, "data: {\"msg\":\"okok\"}\n\n")
-        f.Flush()
-        fmt.Fprintf(w, "data: transmission ended!\n\n")
-        f.Flush()
-        // sends it all at once
-        // double return sends sequential messages
-        // handler call end terminates conn
-        // maintain conn for life of function with
-        // select chan statement / loop
-    }
-}
-
-func CapiPeerHandler(w http.ResponseWriter, r *http.Request) {
-    // collect peer ids
-    // write list to output stream
-    // w.Write([]byte(PEER_MOTD))
-    b0 := new(bytes.Buffer)
-    b0.ReadFrom(r.Body)
-    s0 := string(b0.Bytes())
-    P.Add(s0)
-    b1, err := P.Json()
-    if err != nil {
-        w.Header().Set("Content-Type", "text/plain")
-        s1 := fmt.Sprintf("error: %s\n", err)
-        w.Write([]byte(s1))
-        return
-    }
-    w.Header().Set("Content-Type", "application/json")
-    w.Write(b1)
-}
-
 func init() {
-    P = NewPeers()
-    CS = NewCapis()
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, BADE)
-    })
-    http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("okaq gira karu bade ok!"))
-    })
-    http.HandleFunc("/logs", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("req logged"))
-    })
-    http.HandleFunc("/capi", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, CAPI)
-    })
-    http.HandleFunc("/enko", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, ENKO)
-    })
-    http.HandleFunc("/fago", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, FAGO)
-    })
-    http.HandleFunc("/gugo", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, GUGO)
-    })
-    http.HandleFunc("/hoka", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, HOKA)
-    })
-    http.HandleFunc("/ionu", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, IONU)
-    })
-    http.HandleFunc("/jonu", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, JONU)
-    })
-    http.HandleFunc("/konu", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, KONU)
-    })
-    http.HandleFunc("/lonu", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, LONU)
-    })
     http.HandleFunc("/monu", func(w http.ResponseWriter, r *http.Request) {
         http.ServeFile(w, r, MONU)
     })
-    http.HandleFunc("/capi/init", CapiHandler)
-    http.HandleFunc("/capi/sse", CapiSseHandler)
-    http.HandleFunc("/capi/peer", CapiPeerHandler)
 }
-
-// may initially seed root request handler
-// with template that emits server info, token
-
-// rate limit by ip, url path
-// cache request count using global var
-// with rwlock
-// write limit reached msg if limit hit
-
-
+// connection / hub message pattern
+// channels to broadcast and receive
+// xhr connection to post peer id
+// server side event push
+// once 4 peers have joined
+// then peers negotiate random connections
+// with one local hub
