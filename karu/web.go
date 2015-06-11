ad7dcf03d42bbf059deaecea1cc5866c4bdbe98a
AQ <aq@okaq.com>
2015-06-11 08:09:09 -0400

    New file onku.html. Partizan live game

--- karu/web.go
+++ karu/web.go
@@ -1,7 +1,7 @@
 // golang web server
 // gcloud dev env
 // AQ <aq@okaq.com>
-// 2015-06-08
+// 2015-06-12
 package main
 
 import (
@@ -9,23 +9,11 @@ import (
 )
 
 const (
-    NONU = "nonu.html"
+    ONKU = "onku.html"
 )
 
-func DapiHandler(w http.ResponseWriter, req *http.Request) {
-    w.Write([]byte("greetings xhr"))
-}
-
 func init() {
-    http.HandleFunc("/nonu", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, NONU)
+    http.HandleFunc("/onku", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, ONKU)
     })
-    http.HandleFunc("/dapi", DapiHandler)
 }
-// connection / hub message pattern
-// channels to broadcast and receive
-// xhr connection to post peer id
-// server side event push
-// once 4 peers have joined
-// then peers negotiate random connections
-// with one local hub
