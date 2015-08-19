b874bc34b5ab976f9ea13ea86db87b1187ccf8f7
AQ <aq@okaq.com>
2015-08-19 09:45:20 -0400

    New favicon purple

---
+++ naka/zami.go
@@ -0,0 +1,14 @@
+// okaq naka web app
+// AQ <aq@okaq.com>
+// 2015-08-20
+package main
+
+import (
+    "net/http"
+)
+
+func init() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "bami.html")
+    })
+}
