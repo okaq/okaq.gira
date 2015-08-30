28bac5ca03e3ed7f075ab062516bc2204b6855fb
AQ <aq@okaq.com>
2015-08-30 08:34:08 -0400

    New file dami.html

--- naka/zami.go
+++ naka/zami.go
@@ -9,6 +9,6 @@ import (
 
 func init() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w, r, "cami.html")
+        http.ServeFile(w, r, "dami.html")
     })
 }
