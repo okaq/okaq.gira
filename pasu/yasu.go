d64d4744a7ceeb47e1d77195ef0f797f3f3d34eb
AQ <aq@okaq.com>
2015-09-13 16:33:47 -0400

    Decode and expand oxy bmp font. Typed array, bit array and bit string representations

--- pasu/yasu.go
+++ pasu/yasu.go
@@ -9,10 +9,16 @@ import (
     "net/http"
 )
 
+func YasuHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r)
+    http.ServeFile(w,r,"casu.html")
+}
+
 func main() {
     fmt.Println("yasu web server live...")
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        http.ServeFile(w,r,"casu.html")
-    })
+    http.HandleFunc("/", YasuHandler)
+    // http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        // http.ServeFile(w,r,"casu.html")
+    // })
     http.ListenAndServe(":8008", nil)
 }
