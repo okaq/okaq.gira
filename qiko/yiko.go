a20b846d20fcd89474910475c48e90218fb75955
AQ <aq@okaq.com>
2015-11-24 20:41:34 -0500

    Xhr ping method start player id

--- qiko/yiko.go
+++ qiko/yiko.go
@@ -10,8 +10,14 @@ func YikoHandler(w http.ResponseWriter, r *http.Request) {
     http.ServeFile(w,r,"biko.html")
 }
 
+func PingHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r)
+    w.Write([]byte("ok dues"))
+}
+
 func main() {
     fmt.Println("starting web server")
     http.HandleFunc("/", YikoHandler)
+    http.HandleFunc("/a", PingHandler)
     http.ListenAndServe(":8008", nil)
 }
