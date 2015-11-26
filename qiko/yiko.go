81694189b80a0863244babd2fdaa0b4c1a02473a
AQ <aq@okaq.com>
2015-11-26 10:50:00 -0500

    Send json player id to server and hash

--- qiko/yiko.go
+++ qiko/yiko.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+    "crypto/sha1"
     "encoding/json"
     "fmt"
+    "io/ioutil"
     "net/http"
     "strconv"
     "time"
@@ -37,9 +39,26 @@ func PingHandler(w http.ResponseWriter, r *http.Request) {
 
 func PongHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Println(r)
-    w.Write([]byte("ok ello"))
+    // w.Write([]byte("ok ello"))
     // set rest of values in cache
     // hash all three markers to qid
+    b0, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+        fmt.Println(err)
+    }
+    var q0 Qid
+    err = json.Unmarshal(b0, &q0)
+    if err != nil {
+        fmt.Println(err)
+    }
+    // fmt.Println(q0)
+    h0 := sha1.New()
+    h1 := h0.Sum(b0)
+    // fmt.Printf("%x", h1)
+    // fmt.Println(string(h1))
+    q0.Hash = string(h1)
+    fmt.Println(q0)
+    w.Write(h1)
 }
 
 func main() {
