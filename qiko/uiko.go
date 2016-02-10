d94f3f1c2d96c1a8015aa1b310065b3747b4771e
AQ <aq@okaq.com>
2016-02-09 19:22:57 -0500

    Pid cache setup

--- qiko/uiko.go
+++ qiko/uiko.go
@@ -9,6 +9,11 @@ const (
     FIKO = "fiko.html"
 )
 
+var (
+    Cache []string
+    Rec chan string
+)
+
 func UikoHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Println(r)
     http.ServeFile(w, r, FIKO)
@@ -16,6 +21,12 @@ func UikoHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
     fmt.Println("okaq gir qikko uiko web server started on localhost:8008")
+    // launch goroutine to handle cache
+    // receive on requests to append to cache
+    // request handler sends on global chan
+    
+    // dedicated type for Cache and Receiver
+    // json send and request via fetch
     http.HandleFunc("/", UikoHandler)
     http.ListenAndServe(":8008", nil)
 }
