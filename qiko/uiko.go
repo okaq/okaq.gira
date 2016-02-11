973e328853d18534fe3e52bfc9edb016712f1ad1
AQ <aq@okaq.com>
2016-02-11 10:08:00 -0500

    Binary data post handler

--- qiko/uiko.go
+++ qiko/uiko.go
@@ -3,6 +3,7 @@ package main
 import (
     "encoding/json"
     "fmt"
+    "io/ioutil"
     "net/http"
 )
 
@@ -35,6 +36,21 @@ func QidHandler(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte(s0))
 }
 
+func BidHandler(w http.ResponseWriter, r *http.Request) {
+    // binary post data handler
+    fmt.Println(r)
+    // fmt.Println(r.Body)
+    // w.Write([]byte("bid ok"))
+    // readall for small data streams
+    // use bufio for larger unknown streams
+    b0, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+        fmt.Println(err)
+    }
+    fmt.Println(b0)
+    s0 := fmt.Sprintf("bid: rec %d bytes", len(b0))
+    w.Write([]byte(s0))
+}
 
 func main() {
     fmt.Println("okaq gir qikko uiko web server started on localhost:8008")
@@ -46,5 +62,6 @@ func main() {
     // json send and request via fetch
     http.HandleFunc("/", UikoHandler)
     http.HandleFunc("/qid", QidHandler)
+    http.HandleFunc("/bid", BidHandler)
     http.ListenAndServe(":8008", nil)
 }
